114c5e3ac4c614c791bedd5cf4e38a73d010261f
Paco <pacop0587@gmail.com>
2022-04-25 23:05:06 -0500

    Se agrego el formulario y se agrego estilos a App y Formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import "./App.css";
+//Components
+import Formulario from "./components/Formulario";
+
 function App() {
 	return (
-		<div className="container">
-			<h1>Agenda de contactos</h1>
+		<div className="font-montse color-primary-background vh-100">
+			<div className="container">
+				<h1 className="text-center fw-bold text-primary-color">
+					Agenda de contactos
+				</h1>
+				<div className="row mt-4">
+					<div className="col-6">
+						<Formulario />
+					</div>
+					<div className="col-6">
+						<h2 className="text-center text-primary-color">
+							Lista de contactos
+						</h2>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import "./Formulario.css";
 
 const Formulario = () => {
-	return <div>Formulario</div>;
+	return (
+		<>
+			<h2 className="text-center text-primary-color">Nuevo Contacto</h2>
+			<form action="" className="border rounded p-5 background-secondary-color">
+				<div className="mb-3">
+					<label htmlFor="" className="form-label text-secondary-color fw-bold">
+						Nombre:
+					</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label htmlFor="" className="form-label text-secondary-color fw-bold">
+						Apellido:
+					</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label htmlFor="" className="form-label text-secondary-color fw-bold">
+						Direccion:
+					</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label htmlFor="" className="form-label text-secondary-color fw-bold">
+						Email:
+					</label>
+					<input type="text" className="form-control" />
+				</div>
+				<div className="mb-3">
+					<label htmlFor="" className="form-label text-secondary-color fw-bold">
+						Telefono:
+					</label>
+					<input type="text" className="form-control" />
+				</div>
+				<button
+					type="input"
+					className="color-primary-background btn text-light mt-2"
+				>
+					Guardar
+				</button>
+			</form>
+		</>
+	);
 };
 
 export default Formulario;
