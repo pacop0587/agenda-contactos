c8665f57ae8542c97c365b2b195cf9703fec75e0
Paco <pacop0587@gmail.com>
2022-05-06 17:36:36 -0500

    Se agrego componente Error404 y se estilizo el componente de Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import UseProvider from "./UseProvider";
 import Index from "./views/Index";
 import Login from "./components/Login";
+import Error404 from "./views/Error404";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Index />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<Error404 />} />
 			</Routes>
 		</UseProvider>
 	);
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,9 +2,24 @@ import React from "react";
 
 const Login = () => {
 	return (
-		<>
-			<h2>Bienvenido a Login</h2>
-		</>
+		<div className="container ">
+			<div className="d-flex flex-column justify-content-center align-items-center min-vh-100">
+				<h2 className="">Iniciar Sesion</h2>
+				<form action="" className="d-flex flex-column">
+					<div className="mb-3">
+						<label className="form-label">Usuario</label>
+						<input type="text" className="form-control" />
+					</div>
+					<div className="mb-3">
+						<label className="form-label">Password</label>
+						<input type="text" className="form-control" />
+					</div>
+					<button type="submit" className="btn btn-success">
+						Iniciar Sesion
+					</button>
+				</form>
+			</div>
+		</div>
 	);
 };
 
