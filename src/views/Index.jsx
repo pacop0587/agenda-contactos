33f41d0715044ccaadef129c8c7a84d5d2bff4c9
pacop0587 <47878985+pacop0587@users.noreply.github.com>
2022-05-19 21:32:03 -0500

    Fix styles (#8)
    * Se cambiaron los estilos en Login.jsx
    
    * Se cambiaron los estilos de Register.jsx
    
    * Se agregaron cambios al estilo de formulario

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -8,6 +8,9 @@ import Formulario from "../components/Formulario";
 import Contactos from "../components/Contactos";
 import Header from "../components/Header";
 
+//Styles
+import "../App.css";
+
 //----> End Imports
 
 const index = () => {
@@ -18,22 +21,22 @@ const index = () => {
 	const [modeEdition, setModeEdition] = useState(false);
 
 	return (
-		<div className="font-montse color-primary-background min-vh-100 mw-100">
+		<div className="background-color-second mw-100 mh-100">
 			<div className="container">
 				<Header />
 				<h1 className="text-center fw-bold text-color-normal">
 					Agenda de contactos
 				</h1>
 				<div className="row mt-5">
-					<div className="col-md-6 col-sm-12">
+					<div className="col-sm-12 col-md-6">
 						<Formulario
 							editionContact={editionContact}
 							modeEdition={modeEdition}
 							setModeEdition={setModeEdition}
 						/>
 					</div>
-					<div className="col-md-6 col-sm-12">
-						<h2 className="text-center text-primary-color">
+					<div className="col-sm-12 col-md-6">
+						<h2 className="text-center">
 							<Contactos
 								setEditionContact={setEditionContact}
 								setModeEdition={setModeEdition}
